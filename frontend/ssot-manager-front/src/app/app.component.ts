61f612acd5dc2f22c7fe1a69074d60b1e27f169a
Fernanda Almeida <fvfcanesin@gmail.com>
2026-08-05 11:16:57 -0300

    feat: add Angular Material sidenav layout

--- frontend/ssot-manager-front/src/app/app.component.ts
+++ frontend/ssot-manager-front/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+  RouterOutlet, 
+  HomeComponent,
+  MatSidenavModule,
+  MatListModule,
+  MatToolbarModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
