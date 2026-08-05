d29e81352bf7981e301bcfeb420f6cea6c9045f3
Fernanda Almeida <fvfcanesin@gmail.com>
2026-08-05 10:44:25 -0300

    feat: improve Angular frontend styling with Material theme

--- frontend/ssot-manager-front/src/app/pages/home/home.component.ts
+++ frontend/ssot-manager-front/src/app/pages/home/home.component.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../models/usuario';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -20,10 +31,6 @@ export class HomeComponent implements OnInit {
       .subscribe(dados => {
         this.usuarios = dados;
         console.log(dados);
-
       });
   }
-
- 
-
 }
